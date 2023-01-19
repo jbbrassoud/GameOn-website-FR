d249770e5db6e54831b22ba3b80a6e44785f7c98
jbbrassoud <jbbrassoud@gmail.com>
2023-01-19 12:28:11 +0100

    ISSUE 2 OK ISSUE 3 OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,8 +22,13 @@ const quantityInput = document.querySelector('#quantity');
 const locationInputs = (document.querySelectorAll('.checkbox-input'));
 const conditionsInput = document.querySelector('#checkbox1');
 const newsletterInput = document.querySelector('#checkbox2');
-const ErrorDisplayFirst = document.querySelector('.formErrorFirst')
-const ErrorDisplayLast = document.querySelector('.formErrorLast')
+const ErrorDisplayFirst = document.querySelector('.formErrorFirst');
+const ErrorDisplayLast = document.querySelector('.formErrorLast');
+const ErrorDisplayEmail = document.querySelector('.formErrorEmail');
+const ErrorDisplayBirthdate = document.querySelector('.formErrorBirthdate');
+const ErrorDisplayQuantity = document.querySelector('.formErrorQuantity');
+const ErrorDisplayLocation = document.querySelector('.formErrorLocation');
+const ErrorDisplayConditions = document.querySelector('.formErrorConditions');
 
 // Regexp 
 const nameRegExp = "/^[a-zéèôöîïûùü' -]{2,50}$/i";
@@ -48,16 +53,16 @@ function closeModal() {
 
 
 /*Test de scan auto en fonction imbriquées.
+
 const formScan = document.querySelectorAll('input')
 
 formScan.forEach((input) => {
   input.addEventListener("focus", input => {
-if(querySelectorAll.input.willValidate == true){
-  console.log("ça va être valide")
-} else {
-  querySelector.input.classList.add("inputError");
-}
-    
+    if(querySelectorAll.input.willValidate == true){
+      console.log("ça va être valide")
+    } else {
+      querySelector.input.classList.add("inputError");
+    }
   })
 })
 */
@@ -75,39 +80,52 @@ function verificator(){
     console.log("erreur firstname");
     ErrorDisplayFirst.style.display = "block";
     first.classList.add("inputError");
-  };
+  }
   if (lastNameInput.value.length >= 2){
     console.log("last is good");
     ErrorDisplayLast.style.display = "none";
   } else {
     console.log("error lastname");
     ErrorDisplayLast.style.display = "block";
-  };
-  if (emailInput.validity.typeMismatch != true){
-    console.log("email is good");
-  } else {
-    console.log("email error")
   }
-  if (birthDateInput.validity.typeMismatch != true){
-  console.log("date is good");
+  //email - logique inversée car on cherche une erreur avec le Mismatch
+  if (emailInput.validity.typeMismatch != false){
+    console.log("email error");
+    ErrorDisplayEmail.style.display = "block";
+    email.classList.add("inputError");
   } else {
-    console.log("date error")
+    console.log("email is good");
+  }
+
+  //chercher à trouver nouvelle date ?
+  if (birthDateInput.validity.typeMismatch != true){
+    console.log("date is good");
+  } else {
+    console.log("date error");
+    ErrorDisplayBirthdate.style.display = "block";
+    birthdate.classList.add("inputError");
   }
   if (quantityInput.value.length >= 1){
-  console.log("quantity is good");
+    console.log("quantity is good");
   } else {
-    console.log("quantity error")
+    console.log("quantity error");
+    ErrorDisplayQuantity.style.display = "block";
+    quantity.classList.add("inputError");
   }
-  //if (locationInputs.checked != false){
-    if (location1.checked | location2.checked | location3.checked | location4.checked |location5.checked){
+  //if (locationInputs.checked){
+  if (location1.checked | location2.checked | location3.checked | location4.checked | location5.checked | location6.checked){
     console.log("location is good");
   } else {
-    console.log("location error")
+    console.log("location error");
+    ErrorDisplayLocation.style.display = "block";
+    location1.classList.add("inputError");
   }
   if (conditionsInput.checked != false){
     console.log("checked is good");
   } else {
     console.log("check error");
+    ErrorDisplayConditions.style.display = "block";
+    checkbox1.classList.add("inputError");
   }
 }
 
@@ -121,58 +139,6 @@ function validate() {
 // Afficher erreur
 
 // Validateur
-
-/*
-function verificator(){
-  if (first => 2.length.first){
-    let validationCount = i++;
-  } else {
-    let validationCount = i--;
-  };
-
-  if (last => 2){
-    let validationCount = i++;
-  } else {
-    let validationCount = i--;
-  };
-
-  if (email => 4){
-    let validationCount = i++;
-  } else {
-    let validationCount = i--;
-  };
-
-  if (birthdate == 10){
-    let validationCount = i++;
-  } else {
-    let validationCount = i--;
-  };
-
-  if (radio = checked.input.radio){
-    let validationCount = i++;
-  } else {
-    let validationCount = i--;
-  };
-
-  if (document.getElementsByClassName("checkbox1") = checked){
-    let validationCount = i++;
-  } else {
-    let validationCount = i--;
-  };
-}
-console.log(validationCount);
-
-if (ValidationCount = 6){
-  let validation = true;
-} else {
-  let validation = false;
-  console.log("Il y a une erreur de comptage")
-}
-
-console.log("Tous les passagers sont embarqués !");
-
-
-*/
 
 
 
