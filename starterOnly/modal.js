11efab346802cbdac59b64ff8f8885895c054973
jbbrassoud <jbbrassoud@gmail.com>
2023-01-19 02:39:32 +0100

    ISSUE 2 FINIE - 3 COMMENCEE

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,23 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Form
+const form = document.getElementById('form')
+const firstNameInput = document.querySelector('#first');
+const lastNameInput = document.querySelector('#last');
+const emailInput = document.querySelector('#email');
+const birthDateInput = document.querySelector('#birthdate');
+const quantityInput = document.querySelector('#quantity');
+const locationInputs = (document.querySelectorAll('.checkbox-input'));
+const conditionsInput = document.querySelector('#checkbox1');
+const newsletterInput = document.querySelector('#checkbox2');
+const ErrorDisplayFirst = document.querySelector('.formErrorFirst')
+const ErrorDisplayLast = document.querySelector('.formErrorLast')
+
+// Regexp 
+const nameRegExp = "/^[a-zéèôöîïûùü' -]{2,50}$/i";
+const emailRegExp = "/^[a-z0-9.-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,10}$/i";
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -25,26 +42,80 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// Form
-const form = document.getElementById('form')
-const firstNameInput = document.querySelector('#first');
-const lastNameInput = document.querySelector('#last');
-const emailInput = document.querySelector('#email');
-const birthDateInput = document.querySelector('#birthdate');
-const quantityInput = document.querySelector('#quantity');
-const locationInputs = Array.from(document.querySelectorAll('.location-input'));
-const conditionsInput = document.querySelector('#checkbox1');
-const newsletterInput = document.querySelector('#checkbox2');
 
-// Regexp 
-const nameRegExp = "/^[a-zéèôöîïûùü' -]{2,50}$/i";
-const emailRegExp = "/^[a-z0-9.-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,10}$/i";
 
 // Fonction
 
+
+/*Test de scan auto en fonction imbriquées.
+const formScan = document.querySelectorAll('input')
+
+formScan.forEach((input) => {
+  input.addEventListener("focus", input => {
+if(querySelectorAll.input.willValidate == true){
+  console.log("ça va être valide")
+} else {
+  querySelector.input.classList.add("inputError");
+}
+    
+  })
+})
+*/
+
+function errorMessage(){
+  alert('il y a une erreur');
+  console.log('error message') ;
+}
+
+function verificator(){
+  if (firstNameInput.value.length >= 2){
+    console.log("first is good");
+    ErrorDisplayFirst.style.display = "none";
+  } else {
+    console.log("erreur firstname");
+    ErrorDisplayFirst.style.display = "block";
+    first.classList.add("inputError");
+  };
+  if (lastNameInput.value.length >= 2){
+    console.log("last is good");
+    ErrorDisplayLast.style.display = "none";
+  } else {
+    console.log("error lastname");
+    ErrorDisplayLast.style.display = "block";
+  };
+  if (emailInput.validity.typeMismatch != true){
+    console.log("email is good");
+  } else {
+    console.log("email error")
+  }
+  if (birthDateInput.validity.typeMismatch != true){
+  console.log("date is good");
+  } else {
+    console.log("date error")
+  }
+  if (quantityInput.value.length >= 1){
+  console.log("quantity is good");
+  } else {
+    console.log("quantity error")
+  }
+  //if (locationInputs.checked != false){
+    if (location1.checked | location2.checked | location3.checked | location4.checked |location5.checked){
+    console.log("location is good");
+  } else {
+    console.log("location error")
+  }
+  if (conditionsInput.checked != false){
+    console.log("checked is good");
+  } else {
+    console.log("check error");
+  }
+}
+
+
+
 function validate() {
-    console.log("Merci pour votre inscription");
-  
+  verificator ();
+  console.log("Merci pour votre inscription");
 }
 
 // Afficher erreur
